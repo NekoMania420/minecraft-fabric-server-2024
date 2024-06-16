6b732a687651ea26de37564b3783d21f0b21e0f2
Seksan Neramitthanasombat <sek.cgnz@gmail.com>
2024-06-16 21:07:25 +0700

    feat: custom RAM size

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,4 +15,4 @@ if [ ! -f "server.propreties" ]; then
   cp /server.properties .
 fi
 
-java -jar fabric-server-launch.jar
+sh -c "java -Xms${RAM_SIZE:-2G} -Xmx${RAM_SIZE:-2G} -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -XX:G1MaxNewSizePercent=40 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1NewSizePercent=30 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -XX:MaxGCPauseMillis=200 -XX:MaxTenuringThreshold=1 -XX:SurvivorRatio=32 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar fabric-server-launch.jar nogui"
