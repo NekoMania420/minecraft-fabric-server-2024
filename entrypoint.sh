fc61bcf8202c6ce382a12b6fab5af263d4bc38d2
Seksan Neramitthanasombat <sek.cgnz@gmail.com>
2024-06-17 19:16:01 +0700

    feat: dynamic entrypoint, optimize server.properties

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+ram_size=${RAM_SIZE:-2048}
+g1_new_size_percent=30
+g1_max_new_size_percent=40
+g1_heap_region_size=8
+g1_reserve_percent=20
+
+if [ $ram_size -ge 12288 ]; then
+  g1_new_size_percent=40
+  g1_max_new_size_percent=50
+  g1_heap_region_size=16
+  g1_reserve_percent=15
+fi
+
 if [ ! -f "/server/fabric-server-launch.jar" ]; then
   wget https://maven.fabricmc.net/net/fabricmc/fabric-installer/1.0.1/fabric-installer-1.0.1.jar -O fabric-installer.jar
   java -jar fabric-installer.jar server -dir server -downloadMinecraft
@@ -11,8 +24,8 @@ if [ ! -f "eula.txt" ]; then
   cp /eula.txt .
 fi
 
-if [ ! -f "server.propreties" ]; then
+if [ ! -f "server.properties" ]; then
   cp /server.properties .
 fi
 
-sh -c "java -Xms${RAM_SIZE:-2G} -Xmx${RAM_SIZE:-2G} -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -XX:G1MaxNewSizePercent=40 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1NewSizePercent=30 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -XX:MaxGCPauseMillis=200 -XX:MaxTenuringThreshold=1 -XX:SurvivorRatio=32 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar fabric-server-launch.jar nogui"
+sh -c "java -Xms${ram_size}M -Xmx${ram_size}M --add-modules=jdk.incubator.vector -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -XX:G1NewSizePercent=$g1_new_size_percent -XX:G1MaxNewSizePercent=$g1_max_new_size_percent -XX:G1HeapRegionSize=${g1_heap_region_size}M -XX:G1ReservePercent=$g1_reserve_percent -jar fabric-server-launch.jar --nogui"
